e572966de9d269a7467b1bb4744d15a0c3395c5d
N Rajakumaran <108915966+raja862@users.noreply.github.com>
2024-01-24 14:14:07 +0530

    Update router.js

--- router.js
+++ router.js
@@ -6,7 +6,6 @@ import express from "express"
 
 router.post("/signup" , createUser)
 router.post ("/login" , getUser)
- router.get("/all", getAll)
 router.get("/one/:id", getOne)
 router.put("/update/:id",updateOne)
  export default router;
