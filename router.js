76c990e32bc79463f5adcb81019cccb58e1865c3
N Rajakumaran <108915966+raja862@users.noreply.github.com>
2024-01-24 13:58:47 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,4 +1,4 @@
-import { createUser, getUser,userprofile } from "./control.js";
+import { createUser, getOne, getUser,updateOne} from "./control.js";
 import express from "express"
 
 
@@ -7,6 +7,7 @@ import express from "express"
 
 router.post("/signup" , createUser)
 router.post ("/login" , getUser)
- router.get("/profile",userprofile )
-
+ router.get("/all", getAll)
+router.get("/one/:id", getOne)
+router.put("/update/:id",updateOne)
  export default router;
